0414951b8bf0dcf7cf60c788cc2580279f074599
Alessandro Felaco <113336615+afelaco@users.noreply.github.com>
2025-09-27 20:04:40 +0200

    Use this in definition of resources (#6)
    Co-authored-by: Alessandro Felaco <alessandro.felaco@edgardcooper.com>

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -1,5 +1,5 @@
 # Storage Account
-resource "azurerm_storage_account" "sa" {
+resource "azurerm_storage_account" "this" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
@@ -8,9 +8,9 @@ resource "azurerm_storage_account" "sa" {
 }
 
 # Container
-resource "azurerm_storage_container" "container" {
+resource "azurerm_storage_container" "this" {
   for_each = toset(var.container_name)
 
   name                 = each.key
-  storage_account_name = azurerm_storage_account.sa.name
+  storage_account_name = azurerm_storage_account.this.name
 }
