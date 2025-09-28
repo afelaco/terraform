ce16c70bb55dcb8147e49a5beb6f82fc9c94e274
Alessandro Felaco <alessandro.felaco@edgardcooper.com>
2025-09-28 18:39:34 +0200

    dev

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -10,5 +10,5 @@ resource "azurerm_storage_container" "this" {
   for_each = toset(["steam"])
 
   name               = each.key
-  storage_account_id = azurerm_storage_account.this.id
+  storage_account_name = azurerm_storage_account.this.name
 }
