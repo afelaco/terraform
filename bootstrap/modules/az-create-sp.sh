c64371f20434729f2d6e6daf29d8c3af1df2a185
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-09-29 17:43:57 +0200

    Assign RBAC

--- bootstrap/modules/az-create-sp.sh
+++ bootstrap/modules/az-create-sp.sh
@@ -11,8 +11,30 @@ echo "  ✅ Azure CLI login successful!"
 echo "  ➡️ Creating Azure Service Principal: $AZ_SP_NAME..."
 az ad sp create-for-rbac \
     --name "$AZ_SP_NAME" \
-    --role Contributor \
-    --scopes /subscriptions/"$AZ_SUBSCRIPTION_ID" \
     --sdk-auth > "$AZ_SP_CREDS_FILE"
 
-echo "  ✅ Service Principal created and credentials saved to $AZ_SP_CREDS_FILE!"
+echo "  ✅ Service Principal $AZ_SP_NAME created and credentials saved to $AZ_SP_CREDS_FILE!"
+
+# -----------------------------
+# Assign roles to the Service Principal
+# -----------------------------
+echo "  ➡️ Assigning roles to Service Principal..."
+
+# Extract SP appId from JSON
+AZ_SP_APP_ID=$(jq -r '.clientId' < "$AZ_SP_CREDS_FILE")
+
+# 2️⃣ Assign Contributor role for creating resources
+echo "➡️ Assigning Contributor role to Service Principal $AZ_SP_NAME..."
+az role assignment create \
+    --assignee "$AZ_SP_APP_ID" \
+    --role "Contributor" \
+    --scope /subscriptions/"$AZ_SUBSCRIPTION_ID"
+
+# 3️⃣ Assign Key Vault Contributor role for creating secrets
+echo "➡️ Assigning Key Vault Contributor role to Service Principal $AZ_SP_NAME..."
+az role assignment create \
+    --assignee "$AZ_SP_APP_ID" \
+    --role "Key Vault Contributor" \
+    --scope /subscriptions/"$AZ_SUBSCRIPTION_ID"
+
+echo "✅ Service Principal roles assigned!"
--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -8,18 +8,10 @@ resource "azurerm_key_vault" "this" {
   enable_rbac_authorization = true
 }
 
-# Assign Key Vault Secrets User role to the Service Principal.
-resource "azurerm_role_assignment" "this" {
-  scope                = azurerm_key_vault.this.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = var.object_id
-}
-
 # Set external secrets.
 resource "azurerm_key_vault_secret" "this" {
   for_each = var.external_secrets
 
-  depends_on = [azurerm_role_assignment.this]
   name         = each.key
   value        = each.value
   key_vault_id = azurerm_key_vault.this.id
