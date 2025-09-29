7309151793487971a1c922881d06dd8973efd776
Alessandro Felaco <alessandro.felaco.91@gmail.com>
2025-09-29 19:14:10 +0200

    Least access principle

--- modules/key_vault/variables.tf
+++ modules/key_vault/variables.tf
@@ -25,6 +25,13 @@ variable "admin_object_id" {
   sensitive   = true
 }
 
+variable "sp_object_id" {
+  description = "The Object ID of the admin to assign roles."
+  type        = string
+  default     = "0a909171-669b-492b-bd9b-c1f777a42f23"
+  sensitive   = true
+}
+
 variable "external_secrets" {
   description = "All external secrets to sync to Key Vault"
   type = map(string)
